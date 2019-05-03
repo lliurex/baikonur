40d05f866624bf56a0fa6925166e77f0befd0c04
raurodse <raurodse@gmail.com>
2019-05-03 14:09:01 +0200

    interactive jenkins install

--- jenkins/security.groovy
+++ jenkins/security.groovy
@@ -3,7 +3,14 @@
 import jenkins.model.*
 import hudson.security.*
 import hudson.security.csrf.DefaultCrumbIssuer
+import hudson.slaves.*
+import hudson.slaves.EnvironmentVariablesNodeProperty.Entry
 import jenkins.security.s2m.AdminWhitelistRule
+import com.cloudbees.plugins.credentials.impl.*;
+import com.cloudbees.plugins.credentials.*;
+import org.jenkinsci.plugins.plaincredentials.*
+import org.jenkinsci.plugins.plaincredentials.impl.*
+import com.cloudbees.plugins.credentials.common.*
  
 def instance = Jenkins.getInstance()
 
@@ -74,3 +81,49 @@ xmlStream = new ByteArrayInputStream( file_config.getBytes() )
 
 Jenkins.instance.createProjectFromXML(jobName, xmlStream)
 
+// Create credentials
+
+/ Username and password
+Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(
+CredentialsScope.GLOBAL, // Scope
+"sbuild_credentials", // id
+"Credentials to sbuild", // description
+user, // username
+pass // password
+)
+
+SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
+
+
+// Create Nodes 
+
+ComputerLauncher launcher = new hudson.plugins.sshslaves.SSHLauncher(
+        "host", // Host
+        22, // Port
+        "sbuild_credentials", // Credentials
+        (String)null, // JVM Options
+        (String)null, // JavaPath
+        (String)null, // Prefix Start Slave Command
+        (String)null, // Suffix Start Slave Command
+        (Integer)null, // Connection Timeout in Seconds
+        (Integer)null, // Maximum Number of Retries
+        (Integer)null // The number of seconds to wait between retries
+)
+
+
+Slave agent = new DumbSlave("agent-node", "/home/jenkins", launcher)
+agent.nodeDescription = "Agent node description"
+agent.numExecutors = 1
+agent.labelString = "sbuild_1"
+agent.mode = Node.Mode.NORMAL
+agent.retentionStrategy = new RetentionStrategy.Always()
+
+// List<Entry> env = new ArrayList<Entry>();
+// env.add(new Entry("key1","value1"))
+// env.add(new Entry("key2","value2"))
+// EnvironmentVariablesNodeProperty envPro = new EnvironmentVariablesNodeProperty(env);
+
+// agent.getNodeProperties().add(envPro)
+
+// Create a "Permanent Agent"
+Jenkins.instance.addNode(agent)
