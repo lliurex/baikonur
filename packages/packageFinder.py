cf5d4b4faaad912244f4b22f7b5e83409223d1f5
Raul Rodrigo <raurodse@gmail.com>
2019-09-11 16:24:02 +0200

    open files with encoding utf-8

--- packages/packageFinder.py
+++ packages/packageFinder.py
@@ -20,15 +20,22 @@ class PackageFinder:
         for release in list_distributions:
             if os.path.exists(os.path.join(xxx,release,'Release')):
                 index_file = deb822.Changes( codecs.open( os.path.join( xxx, release, 'Release' ), encoding='utf-8' ) )
-                
+
                 # Extract index files from Release file.
                 list_files = [ w.strip().split(" ")[2] for w in index_file['md5sum'].split("\n") if w.endswith('Packages') ]
 
                 for x in list_files:
-                    all_packages = control.Control(os.path.join(xxx,release,x))
-                    for package in all_packages.paragraphs:
-                        if ( 'equals' in flags_search and needle == package['Package']  ) or \
-                           ('contains' in flags_search and needle in package['Package'] ) or \
-                           ('description' in flags_search and needle in package['Description']):
-                                global_result.append({'package':package,'release':release, 'index':x})
+                    with codecs.open( os.path.join(xxx,release,x), encoding='utf-8') as fd:
+                        for package in deb822.Deb822.iter_paragraphs(fd):
+                            found = False
+                            if 'source' in flags_search and \
+                                ( ( 'Source' in package.keys() and needle == package['Source'] )  or \
+                                  ( 'Source' not in package.keys() and needle == package['Package'] ) ):
+                                found = True
+                            if ( 'equals' in flags_search and needle == package['Package']  ) or \
+                               ('contains' in flags_search and needle in package['Package'] ) or \
+                               ('description' in flags_search and needle in package['Description']):
+                               found = True
+                            if found:
+                                global_result.append({'package':dict(package),'release':release, 'index':x})
         return global_result
