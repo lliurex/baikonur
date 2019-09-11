78f302e58a9f3d016e3805a810ea6cb707c39c21
Raul Rodrigo <raurodse@gmail.com>
2019-09-11 16:25:18 +0200

    append json response

--- packages/packagesbaikonur.py
+++ packages/packagesbaikonur.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, escape, render_template
+from flask import Flask, request, escape, render_template, Response
 from packageFinder import PackageFinder
 import json
 
 application = Flask(__name__)
 configuration = json.load(open('/run/secrets/packagesconfig','r'))
+application.config['JSON_AS_ASCII'] = False
 
 @application.route("/find/")
 def index():
@@ -19,16 +20,24 @@ def search():
         options.append('contains')
     if 'description' in request.form:
         options.append('description')
+    if 'source' in request.form:
+        options.append('source')
+        options.append('equals')
     if 'needle' not in request.form:
         return "Keyword is needed"
     if 'repository' not in request.form:
         return "Repository is needed"
-    app = PackageFinder(configuration['url_base'], configuration['search_path'] +"/" + result['repository'], configuration['pool_path'])
+    app = PackageFinder(configuration['url_base'], result['repository'] + "/" + configuration['search_path'], configuration['pool_path'])
     context = {}
     context['packages'] = app.search(result['needle'],options)
     context['baseurl'] = app.base_url
+    if 'json' in request.form:
+        response = application.response_class(
+                response = json.dumps(context,ensure_ascii=False),
+                mimetype='application/json')
+        return response
     return render_template("result.html", **context)
-    
+
 
 
 if __name__ == "__main__":
